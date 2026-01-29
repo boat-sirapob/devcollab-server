65ce5bb3d1cc3d52826d667d966aa2eefe780cae
Sirapob Sriviriyahphaiboon <boat.sirapob@gmail.com>
2026-01-29 00:56:22 +0000

    feat: add basic logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { Logger } from "@hocuspocus/extension-logger";
 import { Server } from "@hocuspocus/server";
 
 const server = new Server({
   port: 1234,
+  extensions: [new Logger()]
 });
 
 server.listen();
